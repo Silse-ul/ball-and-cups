80713713ab13f55285aaf1decb97486ad9b4f2d8
Silse-ul <siu002@edu.zealand.dk>
2026-02-12 12:08:40 +0100

    balls and cups

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ball_and_cups;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Gæt om bolden ligger under 1,2 eller 3");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Write("#1     ");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write("#2     ");
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("#3");
+        Console.ResetColor();
+        Console.ReadLine();
+
+        Random random = new Random();
+        int cup = random.Next(1, 4);
+        // 1 til 6
+
+
+        string ball; 
+
+        if (cup == 1)
+        {
+            Console.WriteLine("Bolden ligger under nr 1");
+        }
+
+        else
+        if (cup== 2)
+        {
+            Console.WriteLine("Bolden ligger under nr 2");
+        }
+        else
+        if (cup == 3)
+        {
+            Console.WriteLine("Bolden ligger under nr 3");
+        }
+        
+
+
+    }
+}
+
